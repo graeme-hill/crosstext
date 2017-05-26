7cef6f6f976ff33c55148ffd54f573d014e1c9ea
Graeme Hill <graemekh@gmail.com>
2017-05-26 07:28:24 -0700

    Reorganize project structure
    Killed visual studio files because they should be genereted by cmake.
    Moved source files out of src and include and into the root dir.
    Deleted unused CMakeLists.txt.
    
    These changes all primarily to make the project consistent with others.

--- src/build.sh
+++
@@ -1,9 +0,0 @@
-clang++ test.cpp \
-    -o test \
-    -I/usr/include/freetype2 \
-    -I/usr/include/libpng \
-    -L/usr/lib/x86_64-linux-gnu \
-    -lfreetype \
-    -lpng \
-    -std=c++14
-./test
